93b4d074985e64f414906dab27a48575fcc866a9
hughperkins <hughperkins@gmail.com>
2006-12-11 13:20:12 +0000

    Added some interfaces into NetworkInterfaces project

---
+++ Trunk/Source/NetworkInterfaces/ILockRpcToClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkInterfaces
+{
+    public interface ILockRpcToClient
+    {
+        // send result to ILockConsumer, and add lock to locks
+        void LockRequestResponse(object netconnection, IReplicated targetobject, bool LockCreated);
+
+        // if this is one of our locks, signal the ILockConsumer, then mark lock released
+        void LockReleased(object netconnection, IReplicated targetobject);
+    }
+}
