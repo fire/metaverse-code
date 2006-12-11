a030b408d7a77e29b13f35e7ca3875acbb5caf1c
hughperkins <hughperkins@a4456622-f623-0410-966a-81bc31572e38>
2006-12-11 13:20:12 +0000

    Added some interfaces into NetworkInterfaces project

---
+++ Trunk/Source/NetworkInterfaces/ILockRpcToServer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkInterfaces
+{
+    public interface ILockRpcToServer
+    {
+        void RequestLock(object netconnection, IReplicated targetobject);
+        void RenewLock(object netconnection, IReplicated targetobject);
+        void ReleaseLock(object netconnection, IReplicated targetobject);
+    }
+}
---
+++ Trunk/Source/NetworkInterfaces/IObjectReplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetworkInterfaces
+{
+    public interface IObjectReplicationClientToServer
+    {
+        void ObjectCreated( int remoteclientreference, string entitytypename, object entity);
+        void ObjectModified(int reference, string entitytypename, object entity);
+        void ObjectDeleted( int reference);
+    }
+    public interface IObjectReplicationServerToClient
+    {
+        void ObjectCreated(int reference, string entitytypename, object entity);
+        void ObjectModified(int reference, string entitytypename, object entity);
+        void ObjectDeleted(int reference);
+    }
+}
