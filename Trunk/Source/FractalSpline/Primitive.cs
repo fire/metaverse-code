a1f026851f02371cac6fe3d2fe34505024c8beb4
daedius <daedius@a4456622-f623-0410-966a-81bc31572e38>
2008-03-20 15:03:17 +0000

    With Hugh's permission, I have added the LGPL preamble to all of FractalSpline

--- Trunk/Source/FractalSpline/Primitive.cs
+++ Trunk/Source/FractalSpline/Primitive.cs
@@ -1,23 +1,19 @@
-// Copyright Hugh Perkins 2005,2006
+// Copyright Hugh Perkins 2005,2006,2008
 // hughperkins@gmail.com http://manageddreams.com
 //
-// This program is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License version 2 as published by the
-// Free Software Foundation;
+// This library is free software; you can redistribute it and/or modify it 
+// under the terms of the GNU Lesser General Public License as published by 
+// the Free Software Foundation; either version 2.1 of the License, or 
+// (at your option) any later version.
 //
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-//  more details.
+// This library is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public 
+// License for more details.
 //
-// You should have received a copy of the GNU General Public License along
-// with this program in the file licence.txt; if not, write to the
-// Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-
-// 1307 USA
-// You can find the licence also on the web at:
-// http://www.opensource.org/licenses/gpl-license.php
-//
-
+// You should have received a copy of the GNU Lesser General Public 
+// License along with this library; if not, write to the Free Software 
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA  
 using MathGl;
 using System;
 
